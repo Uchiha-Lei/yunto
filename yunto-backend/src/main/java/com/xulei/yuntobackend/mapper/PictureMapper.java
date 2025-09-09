264e5510288d277d825a8ebe7e60ce184fa81d67
许磊 <1677702301@qq.com>
2025-09-09 22:00:19 +0800

    feat(backend）：完成图片模块后端开发 新增腾讯云COS对象存储集成，实现图 询、删除等核心功能。通过集成腾讯云COS 口支持前端调用。

---
+++ yunto-backend/src/main/java/com/xulei/yuntobackend/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.xulei.yuntobackend.mapper;
+
+import com.xulei.yuntobackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author xl
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-09-09 18:12:45
+* @Entity com.xulei.yuntobackend.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
---
+++ yunto-backend/src/main/java/com/xulei/yuntobackend/service/PictureService.java
@@ -0,0 +1,61 @@
+package com.xulei.yuntobackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xulei.yuntobackend.model.VO.PictureVO;
+import com.xulei.yuntobackend.model.dto.picture.PictureQueryRequest;
+import com.xulei.yuntobackend.model.dto.picture.PictureUploadRequest;
+import com.xulei.yuntobackend.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xulei.yuntobackend.model.entity.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xl
+ * @description 针对表【picture(图片)】的数据库操作Service
+ * @createDate 2025-09-09 18:12:45
+ */
+public interface PictureService extends IService<Picture> {
+
+    /**
+     * 上传图片
+     *
+     * @param multipartFile        文件
+     * @param pictureUploadRequest 上传请求体
+     * @param loginUser            登录的用户
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser);
+
+
+    /**
+     * 分页查询图片
+     *
+     * @param pictureQueryRequest 图片查询请求
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    /**
+     * 获取图片
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 分页获取图片封装
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 图片数据校验
+     *
+     * @param picture
+     */
+    void validPicture(Picture picture);
+}
